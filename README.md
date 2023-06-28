d1341fc4216ca7d6661c20fb327c780bf38e36cd
Niranjan-KS <131178323+niranjan-ks-pro@users.noreply.github.com>
2023-06-28 19:50:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# YesTechCardWidgetExerciseSocialMediaPostPart14
+ https://www.youtube.com/watch?v=0_XFAYmp6fc&list=PL1JrLEBAapUWv_JSolgE5pdcDKWw80wKA&index=14
---
+++ android/app/src/main/kotlin/com/example/yestechcardwidgetexercisesocialmediapostpart14/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yestechcardwidgetexercisesocialmediapostpart14
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
